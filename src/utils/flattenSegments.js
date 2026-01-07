c7a7560b312143142da35be584355c4fc0c2ebd1
Cristian <carincon93@gmail.com>
2026-01-07 09:35:40 -0500

    refactor: Add index to each track. Check if segments exist.

--- src/utils/flattenSegments.js
+++ src/utils/flattenSegments.js
@@ -1,7 +1,12 @@
 export function flattenSegments(segments) {
+    if (!segments) return [];
+    let index = -1;
     return segments.flatMap(
-        segment => segment.tracks.map(
-            track => ({ ...track, artistName: track.artist.name, segmentIndex: segment.segmentIndex })
+        (segment) => segment.tracks.map(
+            (track) => {
+                index++;
+                return { ...track, artistName: track.artist.name, segmentIndex: segment.segmentIndex, index }
+            }
         )
     );
 }
